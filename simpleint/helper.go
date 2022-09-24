748389845b28074a591e9baf9e8359e40dde981c
Ali Movahedi <movahedi.4li@gmail.com>
2022-09-24 12:04:52 +0330

    add scan and value for sql

--- simpleint/helper.go
+++ simpleint/helper.go
@@ -1,6 +1,9 @@
 package simpleint
 
-import "fmt"
+import (
+	"database/sql/driver"
+	"fmt"
+)
 
 func (x Int) Text(base int) string {
 	return x.bigInt.Text(base)
@@ -38,6 +41,31 @@ func (x *Int) Format(s fmt.State, ch rune) {
 	x.bigInt.Format(s, ch)
 }
 
-func (x *Int) Scan(s fmt.ScanState, ch rune) error {
-	return x.bigInt.Scan(s, ch)
+func (x *Int) Scan(dbValue interface{}) error {
+	switch src := dbValue.(type) {
+	case nil:
+		return nil
+	case string:
+		if src == "" {
+			return nil
+		}
+
+		stringValue, ok := dbValue.(string)
+		if !ok {
+			return fmt.Errorf("Scan: unable to cast type %T into string in BigNumber", src)
+		}
+
+		_, ok = x.SetString(stringValue, 10)
+		if !ok {
+			return fmt.Errorf("Scan: unable to set string to big.int in BigNumber, source: %s", src)
+		}
+
+		return nil
+	default:
+		return fmt.Errorf("Scan: unable to scan type %T into BigNumber", src)
+	}
+}
+
+func (x Int) Value() (driver.Value, error) {
+	return x.String(), nil
 }
